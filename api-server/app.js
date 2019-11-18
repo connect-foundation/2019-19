0992d6b153ea59ed469c9efda59d34a34a954236
jngsoo <wt2933@icloud.com>
2019-11-18 13:51:12 +0900

    Add: cors 사용
    react와 express의 각각 다른 포트에 접근하기 위해 npm cors 설치 및 사용

--- api-server/app.js
+++ api-server/app.js
@@ -1,12 +1,15 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
