06c77fcf0675981d1ec149ac2a6947dc1f4a4afd
minsung1129 <sju02135@hanmail.net>
2019-11-19 18:40:11 +0900

    Add: context API 구현
    createContext, provider, consumer사용하여 구현
    prop-types로 type 정의
    
    * Add: 임의의 이미지 데이터 추가
    
    picum.photos에서 특정 id와 크기로 랜덤이미지 불러오기
    
    * Add: slider, item 컴포넌트 구현
    
    classnames 설치
    useState, useEffect, useref사용하여 구현
    context API의 producer, consumer적용
    
    * Add: scss적용
    
    scss적용하지 않았던 파일들에 대해 scss적용

--- client-server/src/Pages/PopularPage.js
+++ client-server/src/Pages/PopularPage.js
@@ -1,9 +1,59 @@
 import React from 'react';
+import Slider from '../components/Carousels/NetflixSlider';
+
+const ImgUrlJpg = id => `https://picsum.photos/id/${id}/341/192`;
+const ImgUrlWebp = id => `https://picsum.photos/id/${id}/848/477`;
+
+const movies = [
+  {
+    id: 1,
+    image: ImgUrlJpg(1),
+    imageBg: ImgUrlWebp(1),
+    title: '1983',
+  },
+  {
+    id: 2,
+    image: ImgUrlJpg(2),
+    imageBg: ImgUrlWebp(2),
+    title: 'Russian doll',
+  },
+  {
+    id: 3,
+    image: ImgUrlJpg(3),
+    imageBg: ImgUrlWebp(3),
+    title: 'The rain',
+  },
+  {
+    id: 4,
+    image: ImgUrlJpg(4),
+    imageBg: ImgUrlWebp(4),
+    title: 'Sex education',
+  },
+  {
+    id: 5,
+    image: ImgUrlJpg(5),
+    imageBg: ImgUrlWebp(5),
+    title: 'Elite',
+  },
+  {
+    id: 6,
+    image: ImgUrlJpg(6),
+    imageBg: ImgUrlWebp(6),
+    title: 'Black mirror',
+  },
+];
 
 const Popular = () => {
+  console.log('start here');
   return (
     <>
-      <h1>pop</h1>
+      <Slider>
+        {movies.map(movie => (
+          <Slider.Item movie={movie} key={movie.id}>
+            item1
+          </Slider.Item>
+        ))}
+      </Slider>
     </>
   );
 };
---
+++ client-server/src/components/Carousels/NetflixSlider/Item.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import SliderContext from './context';
+import ShowDetailsButton from './ShowDetailsButton';
+import Mark from './Mark';
+import './Item.scss';
+
+const Item = ({ movie }) => (
+  <SliderContext.Consumer>
+    {({ onSelectSlide, currentSlide, elementRef }) => {
+      const isActive = currentSlide && currentSlide.id === movie.id;
+
+      return (
+        <div
+          ref={elementRef}
+          className={cx('item', {
+            'item--open': isActive,
+          })}
+        >
+          <img src={movie.image} alt="" />
+          <ShowDetailsButton onClick={() => onSelectSlide(movie)} />
+          {isActive && <Mark />}
+        </div>
+      );
+    }}
+  </SliderContext.Consumer>
+);
+
+Item.propTypes = {
+  movie: PropTypes.string.isRequired,
+};
+
+export default Item;
