d8e9c19b46338c8d789cf9dadbe4657f803f682d
jngsoo <wt2933@icloud.com>
2019-11-21 18:34:50 +0900

    Fix: 불필요한 내용 수정

--- client-server/src/Pages/Popular.js
+++ client-server/src/Pages/Popular.js
@@ -7,9 +7,7 @@ const Popular = () => {
     <>
       <Slider>
         {movies.map(movie => (
-          <Slider.Item movie={movie} key={movie.id}>
-            item1
-          </Slider.Item>
+          <Slider.Item movie={movie} key={movie.id} />
         ))}
       </Slider>
     </>
