33a4b11c688a5ff5cb5f1aad250b0f708e73b453
jngsoo <wt2933@icloud.com>
2019-11-25 11:10:44 +0900

    Delete: Login Component 삭제
    로그인 페이지(버튼)이 구글 로그인 페이지로 바로 리다이렉트 되므로
    불필요한 컴포넌트인 Login page component 삭제

--- client-server/src/components/Routes.js
+++ client-server/src/components/Routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Recent from '../Pages/Recent';
-import Login from '../Pages/Login';
 import Home from '../Pages/Home';
 import Popular from '../Pages/Popular';
 // eslint-disable-next-line import/no-cycle
@@ -12,7 +11,6 @@ export default () => (
     <Navbar />
     <Route path="/Popular" component={Popular} />
     <Route path="/Recent" component={Recent} />
-    <Route path="/Login" />
     <Route exact path="/" component={Home} />
   </Router>
 );
