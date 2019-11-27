f6a66e4faed5642e78131a2b635220470e6a7414
zoomspeed77 <zoomspeed77@gmail.com>
2019-11-27 18:29:37 +0900

    Add: ElasticSearch 검색 API 구현 get_search 함수에서  응답에 대한 요청 반환함

--- elastic-server/src/utils/query.js
+++ elastic-server/src/utils/query.js
@@ -55,6 +55,35 @@ async function get_category(column, order, size, category_list){
 }
 
 
+
+async function get_search(column, order, size,category_list,target){
+ 
+
+    //console.log("@@@@@  ",target,column,size,order);
+    return await client.search({
+        index: process.env.index,
+        type: 'data',
+        sort : [`${column}:${order}`],
+        body: {
+            size : size,
+            query: {
+                match : {
+                    name : 'Quisquam voluptas odit'
+                }
+            
+            }
+        }
+    }).then(function(resp){
+        return resp;
+    },function(err){
+        return err;
+    })
+
+};
+
+
+
 module.exports.filter_pipe = filter_pipe;
 module.exports.get_filtering = get_filtering;
 module.exports.get_category = get_category;
+module.exports.get_search = get_search;
