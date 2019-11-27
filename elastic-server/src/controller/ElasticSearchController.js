4be27fd2f9488819bae8558ba50be0f25e8b95ec
zoomspeed77 <zoomspeed77@gmail.com>
2019-11-27 18:30:52 +0900

    Add: 검색 Controller기능 구현 검색 요청에 따라 query의 get_search 호출함

--- elastic-server/src/controller/ElasticSearchController.js
+++ elastic-server/src/controller/ElasticSearchController.js
@@ -29,6 +29,22 @@ const FilterController = {
 
     }
 };
+
+
+const SearchController = {
+
+    get_result: async function(column,order,target){
+
+        let resp = "";
+        resp = await query.filter_pipe(column,order,query.get_search, filter_Size, null,target);
+        console.log(resp);
+    }
+
+    
+}
+
+//SearchController.get_result(es_name,"asc",'Aspernatur veritatis quia');
+
 //FilterController.filter_controller(es_category,"desc",['스포츠','게임']);
 //FilterController.filter_controller(es_videoId,"asc");   //비디오 번호순
 //FilterController.filter_controller(es_likes,"desc");    //좋아요 순
