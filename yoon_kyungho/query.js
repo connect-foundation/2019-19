2a6252b2f32416ac5e6ddc82f3a6aa99f7481ff4
zoomspeed77 <zoomspeed77@gmail.com>
2019-11-29 10:36:01 +0900

    Add: 컨텐츠 필터링,검색 쿼리 구현

---
+++ yoon_kyungho/query.js
@@ -0,0 +1,89 @@
+const {client} = require('./elastic_connectior.js');
+
+const filter_pipe = async (column,order,func,size, category_list,target) =>{
+    let result = await func(column,order,size, category_list,target);
+
+    return result.hits.hits;
+}
+
+async function get_filtering(column, order, size){
+ 
+    return await client.search({
+        index: process.env.index,
+        type: 'data',
+        sort : [`${column}:${order}`],
+        body: {
+            size : size,
+            query: {
+                match_all: {}
+            
+            }
+        }
+    }).then(function(resp){
+        return resp;
+    },function(err){
+        return err;
+    })
+
+};
+
+async function get_category(column, order, size, category_list){
+
+    return await client.search({
+        index: process.env.index,
+        type: 'data',
+        //sort : [`${column}:${order}`],
+        body: {
+            size : size,
+            query: {                
+                match: {
+                    category : {
+                        
+                        query : category_list.join(" "),
+                        operator : "or"
+                        
+                    }
+                }
+            
+            }
+        }
+    }).then(function(resp){
+        return resp;
+    },function(err){
+        return err;
+    })
+}
+
+
+
+async function get_search(column, order, size,category_list,target){
+ 
+
+    //console.log("@@@@@  ",target,column,size,order);
+    return await client.search({
+        index: process.env.index,
+        type: 'data',
+        sort : [`${column}:${order}`],
+        body: {
+            size : size,
+            query: {
+                match : {
+                    name : 'Quisquam voluptas odit'
+                }
+            
+            }
+        }
+    }).then(function(resp){
+        return resp;
+    },function(err){
+        return err;
+    })
+
+};
+
+
+
+module.exports.filter_pipe = filter_pipe;
+module.exports.get_filtering = get_filtering;
+module.exports.get_category = get_category;
+module.exports.get_search = get_search;
