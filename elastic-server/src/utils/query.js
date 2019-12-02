b8d948c4bd071c6829170c178d199367ae9f1fa0
zoomspeed77 <zoomspeed77@gmail.com>
2019-12-02 16:09:25 +0900

    Fix: select, filtering 함수 리팩토링 filter_pipe 함수를 제거하고 controller에서 바로 호출 가능하게 수정

--- elastic-server/src/utils/query.js
+++ elastic-server/src/utils/query.js
@@ -1,110 +1,12 @@
-const { client } = require('./elastic_connection.js');
-
-const filterPipe = async (column, order, func, size, categoryList, target) => {
-  const result = await func(column, order, size, categoryList, target);
-
-  return result.hits.hits;
-};
-
-async function getFiltering(column, order, size) {
-  const data = await client
-    .search({
-      index: process.env.index,
-      type: 'data',
-      sort: [`${column}:${order}`],
-      body: {
-        size,
-        query: {
-          match_all: {},
-        },
-      },
-    })
-    .then(
-      function(resp) {
-        return resp;
-      },
-      function(err) {
-        return err;
-      },
-    );
-  return data;
-}
-
-async function getCategory(column, order, size, categoryList) {
-  const data = await client
-    .search({
-      index: process.env.index,
-      type: 'data',
-      sort: [`${column}:${order}`],
-      body: {
-        size,
-        query: {
-          match: {
-            category: {
-              query: categoryList.join(' '),
-              operator: 'or',
-            },
-          },
-        },
-      },
-    })
-    .then(
-      function(resp) {
-        return resp;
-      },
-      function(err) {
-        return err;
-      },
-    );
-  return data;
-}
-
-async function getSearch(column, order, size, categoryList, target) {
-  // console.log("@@@@@  ",target,column,size,order);
-  const data = await client
-    .search({
-      index: process.env.index,
-      type: 'data',
-      sort: [`${column}:${order}`],
-      body: {
-        size,
-        query: {
-          match: {
-            name: 'Quisquam voluptas odit',
-          },
-        },
-      },
-    })
-    .then(
-      function(resp) {
-        return resp;
-      },
-      function(err) {
-        return err;
-      },
-    );
-  return data;
-}
-
-module.exports.filterPipe = filterPipe;
-module.exports.getFiltering = getFiltering;
-module.exports.getCategory = getCategory;
-module.exports.getSearch = getSearch;
-=======
 const {client} = require('./elastic_connection.js');
 
-const filter_pipe = async (column,order,func,size, category_list,target) =>{
-    let result = await func(column,order,size, category_list,target);
-
-    return result.hits.hits;
-}
 
 async function get_filtering(column, order, size){
  
     return await client.search({
         index: process.env.index,
-        type: 'data',
-        sort : [`${column}:${order}`],
+        type: '_doc',
+        sort : [`${column} : ${order}`],
         body: {
             size : size,
             query: {
@@ -113,19 +15,20 @@ async function get_filtering(column, order, size){
             }
         }
     }).then(function(resp){
-        return resp;
+        return resp.hits.hits;
     },function(err){
         return err;
     })
 
 };
 
-async function get_category(column, order, size, category_list){
+
+async function get_category(category_list,order,size){
 
     return await client.search({
         index: process.env.index,
-        type: 'data',
-        //sort : [`${column}:${order}`],
+        type: '_doc',
+        sort : [`category : ${order}`],
         body: {
             size : size,
             query: {                
@@ -141,42 +44,37 @@ async function get_category(column, order, size, category_list){
             }
         }
     }).then(function(resp){
-        return resp;
+        return resp.hits.hits;
     },function(err){
         return err;
     })
 }
 
 
+async function get_search(column, target, order, size){
 
-async function get_search(column, order, size,category_list,target){
- 
-
-    //console.log("@@@@@  ",target,column,size,order);
     return await client.search({
         index: process.env.index,
-        type: 'data',
-        sort : [`${column}:${order}`],
+        type: '_doc',
+        sort : [`${column} : ${order}`],
         body: {
             size : size,
             query: {
                 match : {
-                    name : 'Quisquam voluptas odit'
+                    [column] : target
                 }
+                
             
             }
         }
     }).then(function(resp){
-        return resp;
+        return resp.hits.hits;
     },function(err){
         return err;
     })
 
 };
 
-
-
-module.exports.filter_pipe = filter_pipe;
 module.exports.get_filtering = get_filtering;
 module.exports.get_category = get_category;
 module.exports.get_search = get_search;
