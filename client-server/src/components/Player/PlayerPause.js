a1bdb70c6258d7a3c9e48829838e790c3f3105c4
KKambi <37494867+KKambi@users.noreply.github.com>
2019-12-04 12:58:26 +0900

    Add: pause 버튼 구현 및 플레잉 상태에 따라 아이콘 변경
    - 플레이어의 play/pause 교체 구현

---
+++ client-server/src/components/Player/PlayerPause.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PlayerPause = () => {
+  return (
+    <>
+      <rect x="9" y="6" width="4" height="16" />
+      <rect x="15" y="6" width="4" height="16" />
+    </>
+  );
+};
+
+export default PlayerPause;
---
+++ client-server/src/components/Player/PlayerPlay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PlayerPlay = () => {
+  return <polygon points="8 22 8 6 22.0043763 14" />;
+};
+
+export default PlayerPlay;
