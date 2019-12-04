8aa8c58063514f073e204e402ea9c3dcc16dc48c
KKambi <37494867+KKambi@users.noreply.github.com>
2019-12-04 09:49:42 +0900

    Add: styled component를 이용한 global style 적용
    - styled component 기반으로 스타일링을 한다면 reset.css 등은 이렇게
    구현해야 함

--- client-server/src/Root.js
+++ client-server/src/Root.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import './index.css';
-import Routes from './components/Routes';
+// import './index.css';
+import { createGlobalStyle } from 'styled-components';
 import { NavbarProvider } from './contexts/NavbarContext';
+import Routes from './components/Routes';
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Noto Sans KR';
+    font-weight: 400;
+    font-style: normal;
+    src: url("./styles/fonts/NotoSansKR-Regular.otf");
+  }
+
+  @font-face {
+    font-family: 'Noto Sans KR';
+    font-weight: 700;
+    font-style: bold;
+    src: url("./styles/fonts/NotoSansKR-Bold.otf");
+  }
+
+  body {
+    background-color: rgb(20, 20, 20);
+    padding: 0;
+    margin: 0;
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+`;
 
 const Root = () => {
   return (
-    <NavbarProvider>
-      <Routes />
-    </NavbarProvider>
+    <>
+      <GlobalStyles />
+      <NavbarProvider>
+        <Routes />
+      </NavbarProvider>
+    </>
   );
 };
 
