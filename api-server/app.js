b6aa37e13a9033289883853eab4668394f632f43
KKambi <37494867+KKambi@users.noreply.github.com>
2019-12-06 11:22:56 +0900

    Update: app.js coding convention update
    - eslint에 의한 업데이트

--- api-server/app.js
+++ api-server/app.js
@@ -2,15 +2,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
-const cors = require('cors');
-
 const app = express();
 app.use(cors());
-
 
 app.use(logger('dev'));
 app.use(express.json());
