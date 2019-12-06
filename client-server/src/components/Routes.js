4971b3646ee7c4fcb8918423e3604bd550d7b5b4
KKambi <37494867+KKambi@users.noreply.github.com>
2019-12-06 11:15:48 +0900

    Update: Pages/Login.js 삭제에 따른 라우트 변경
    - Login Page는 필요없음

--- client-server/src/components/Routes.js
+++ client-server/src/components/Routes.js
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Route, useHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Recent from '../Pages/Recent';
-import Login from '../Pages/Login';
 import Home from '../Pages/Home';
 import Popular from '../Pages/Popular';
 // eslint-disable-next-line import/no-cycle
@@ -17,7 +16,6 @@ const Routes = () => {
       {showNav && <Navbar />}
       <Route path="/Popular" component={Popular} />
       <Route path="/Recent" component={Recent} />
-      <Route path="/Login" component={Login} />
       <Route exact path="/" component={Home} />
       <Route path="/Player/:videoId" component={Player} />
     </Router>
