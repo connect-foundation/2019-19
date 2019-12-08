006b5858fdc3d28b2a0fe89a26dde00976b9b046
minsung1129 <sju02135@hanmail.net>
2019-12-08 17:58:37 +0900

    Fix: 크롤링 파일 수정
    필요없는 파일 삭제 및 변경
    크롤링한 데이터 db에 저장

--- api-server/seeders/20191114015659-demo-videos.js
+++ api-server/seeders/20191114015659-demo-videos.js
@@ -1,41 +1,9 @@
-const casual = require('casual');
-
-const videoCategoryDomain = [
-  '스포츠',
-  '과학기술',
-  '여행',
-  '음악',
-  '엔터테인먼트',
-  '교육',
-  '코미디',
-  '뷰티/패션',
-  '영화/애니메이션',
-  '게임',
-  '노하우/스타일',
-  '뉴스/정치',
-  '애완동물/동물',
-];
-
-const demoData = [];
-for (let i = 0; i < 200; i += 1) {
-  const videoObj = {
-    video_id: null,
-    name: casual.title,
-    category: casual.random_element(videoCategoryDomain),
-    likes: casual.integer(1, 500),
-    reg_date: new Date(Date.now()),
-    thumbnail_img_url: `https://picsum.photos/id/${i}/1600/640`,
-    thumbnai_video_url: null,
-    streaming_url: null,
-  };
-  demoData.push(videoObj);
-}
+const data = require('../../crawler/infoJson/file0.json');
 
 module.exports = {
   up: queryInterface => {
-    return queryInterface.bulkInsert('videos', demoData, {});
+    return queryInterface.bulkInsert('videos', data, {});
   },
-
   down: queryInterface => {
     return queryInterface.bulkDelete('videos', null, {});
   },
