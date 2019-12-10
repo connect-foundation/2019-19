54cffa791721141f2ce43dbba99097a392380b5c
zoomspeed77 <zoomspeed77@gmail.com>
2019-12-10 19:34:07 +0900

    Fix: controller에서 제대로 호출 안되는 버그 수정 parameter 위치수정, 쓰지않는 변수 제거

--- elastic-server/src/controller/ElasticSearchController.js
+++ elastic-server/src/controller/ElasticSearchController.js
@@ -6,48 +6,41 @@ const es_category = process.env.es_category;
 const es_name = process.env.es_name;
 const query = require('../utils/query.js');
 
-const FILTER_SIZE = 40;
-const SEARCH_SIZE = 40;
+
 
 const FilterController = {
-
-    filter_controller : async function(column,order) {
-
-        let resp = null;
-
-        if(column===es_videoId || column===es_likes){
-
-            resp = await query.get_filtering(column, order, FILTER_SIZE);
-        }
-        else if(target==es_category){
-
-            resp = await query.get_category(column, order, FILTER_SIZE);
-        }
-        
-        return resp._index;
+  FILTER_SIZE : 40,
+  filter_controller : async function(column,order,category_list) {
+    
+    let resp = null;
+    if(column===es_videoId || column===es_likes || column==es_name){
+      resp = await query.get_filtering(column, order, this.FILTER_SIZE);
+    }
+    else if(column==es_category){
+      resp = await query.get_category(category_list, order, this.FILTER_SIZE);
+    }
+    console.log(resp);
+    return resp;
 
     }
 };
 
 const SearchController = {
-
-    get_result: async function(column,target,order){
-
-        let resp = "";
-        resp = await query.get_search(column,target,order,SEARCH_SIZE);
-        
-        
-        return resp;
-    }
-
+  
+  SEARCH_SIZE : 40,
+  get_result: async function(column,target,order){
     
+    let resp = "";
+    resp = await query.get_search(column,target,order,this.SEARCH_SIZE);
+        
+    console.log(resp);
+    return resp;
+  }  
 }   
-//console.log(query.get_filtering("name","asc",));
-//console.log(query.get_category(['게','코']),'asc');
-//console.log(query.get_search("category","게","asc",SEARCH_SIZE));
-//console.log(query.get_search("name","Au",FILTER_SIZE));
 
-console.log(SearchController.get_result("name","Au","asc"));
+//console.log(SearchController.get_result("name","Au","asc"));
 //console.log(SearchController.get_result('category','게','asc'));
-//console.log(FilterController.filter_controller(['게','코']),'asc');
+//console.log(FilterController.filter_controller('category','asc',['음악']));
 //console.log(FilterController.filter_controller('name','asc'));
+
+module.exports.FilterController = FilterController;
