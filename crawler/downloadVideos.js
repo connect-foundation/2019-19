9e0ce587959b7da542ac8d5ab5e9f66a905c34e4
minsung1129 <sju02135@hanmail.net>
2019-12-10 14:31:26 +0900

    Fix: 구조 수정

--- crawler/downloadVideos.js
+++ crawler/downloadVideos.js
@@ -2,49 +2,55 @@ const youtubedl = require("youtube-dl");
 const fs = require("fs");
 const data = require("./data.js");
 
-const output = `../../videos/hi.mp4`;
+Object.values(data).forEach((category, categoryIndex) => {
+  category.forEach((urls, urlIndex) => {
+    const output = `../../videos/${
+      Object.keys(data)[categoryIndex]
+    }${urlIndex}.mp4`;
 
-let downloaded = 0;
+    let downloaded = 0;
 
-if (fs.existsSync(output)) {
-  downloaded = fs.statSync(output).size;
-}
+    if (fs.existsSync(output)) {
+      downloaded = fs.statSync(output).size;
+    }
 
-const video = youtubedl(
-  "https://www.youtube.com/watch?v=-qn50cjo-NI",
+    const video = youtubedl(
+      urls,
 
-  // Optional arguments passed to youtube-dl.
-  ["--format=18"],
+      // Optional arguments passed to youtube-dl.
+      ["--format=18"],
 
-  // start will be sent as a range header
-  { start: downloaded, cwd: __dirname }
-);
+      // start will be sent as a range header
+      { start: downloaded, cwd: __dirname }
+    );
 
-// Will be called when the download starts.
-video.on("info", function(info) {
-  console.log("Download started");
-  console.log(`filename: ${info._filename}`);
+    // Will be called when the download starts.
+    video.on("info", function(info) {
+      console.log("Download started");
+      console.log(`filename: ${info._filename}`);
 
-  // info.size will be the amount to download, add
-  const total = info.size + downloaded;
-  console.log(`size: ${total}`);
+      // info.size will be the amount to download, add
+      const total = info.size + downloaded;
+      console.log(`size: ${total}`);
 
-  if (downloaded > 0) {
-    // size will be the amount already downloaded
-    console.log(`resuming from: ${downloaded}`);
+      if (downloaded > 0) {
+        // size will be the amount already downloaded
+        console.log(`resuming from: ${downloaded}`);
 
-    // display the remaining bytes to download
-    console.log(`remaining bytes: ${info.size}`);
-  }
-});
+        // display the remaining bytes to download
+        console.log(`remaining bytes: ${info.size}`);
+      }
+    });
 
-video.pipe(fs.createWriteStream(output, { flags: "a" }));
+    video.pipe(fs.createWriteStream(output, { flags: "a" }));
 
-// Will be called if download was already completed and there is nothing more to download.
-video.on("complete", function complete(info) {
-  console.log(`filename: ${info._filename} already downloaded.`);
-});
+    // Will be called if download was already completed and there is nothing more to download.
+    video.on("complete", function complete(info) {
+      console.log(`filename: ${info._filename} already downloaded.`);
+    });
 
-video.on("end", function() {
-  console.log("finished downloading!");
+    video.on("end", function() {
+      console.log("finished downloading!");
+    });
+  });
 });
