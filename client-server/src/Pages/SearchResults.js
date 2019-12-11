d80b20914ab4b9810e77151c5dc178fe148807a4
jngsoo <wt2933@icloud.com>
2019-12-11 11:34:34 +0900

    Fix: 검색어 재입력시 페이지 랜더링 안되는 버그 해결
    검색어 요청을 보내기 전 onLoading 상태를 true로 초기화 한 후, 검색어 기반 데이터를 받으면 이를 다시 false로
    상태를 바꾸는 방식으로 해결

--- client-server/src/Pages/SearchResults.js
+++ client-server/src/Pages/SearchResults.js
@@ -1,66 +1,80 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import LoginContext from '../loginContextApi/context';
 import Slider from '../components/Carousels/NetflixSlider';
+import styled from 'styled-components';
+
 import ENV from '../../env';
 
 const { apiServer } = ENV;
 
+const MessageOnCenter = styled.h1`
+  position: absolute;
+  color: white;
+  left: 50%;
+  top: 40%;
+  transform: translate(-50%, -50%);
+`;
+
 const SearchResults = props => {
-  const { userInfo } = useContext(LoginContext);
   const [myVideoList, setMyVideoList] = useState([]);
-  const [toBeRendered, setToBeRendered] = useState([]);
   const [onLoading, setOnLoading] = useState(true);
   const [numOfContentsInEachRaw, setNumOfContentsInEachRaw] = useState(5);
   const [sliceIndexArray, setSliceIndexArray] = useState([]);
+  const [noContents, setNoContents] = useState(false);
 
   const { keyword } = props.match.params;
 
   useEffect(() => {
-    if (userInfo) {
-      axios.get(`${apiServer}/video/${keyword}`).then(res => {
-        console.log(res.data.length);
-        if (res.data.length > 0) {
-          console.log(res);
-          setMyVideoList(res.data);
-          const numOfMyVideos = res.data.length;
-          if (res.data.length === 0) return; // 유저가 찜한 컨텐츠가 없다는 view 표시해야
-          const raws = Math.ceil(numOfMyVideos / numOfContentsInEachRaw);
-          const temp = [];
-          for (let i = 0; i < raws; i++) {
-            temp.push(i);
-          }
-          setSliceIndexArray(temp);
-          setOnLoading(false);
+    setOnLoading(true);
+    axios.get(`${apiServer}/video/search/${keyword}`).then(res => {
+      if (res.data.length) {
+        const numOfMyVideos = res.data.length;
+        // 유저가 찜한 컨텐츠가 없다는 view 표시해야
+        const raws = Math.ceil(numOfMyVideos / numOfContentsInEachRaw);
+        let temp = [];
+        for (let i = 0; i < raws; i++) {
+          temp.push(i);
         }
-      });
-    }
-  }, [userInfo, keyword]);
+        setSliceIndexArray(temp);
+        setMyVideoList(res.data);
+        setOnLoading(false);
+        setNoContents(false);
+      } else {
+        setNoContents(true);
+        setOnLoading(false);
+      }
+    });
+  }, [keyword]);
 
   if (onLoading) return null;
 
   return (
     <>
-      {sliceIndexArray.map(e => {
-        return (
-          <Slider>
-            {myVideoList
-              .slice(
-                e * numOfContentsInEachRaw,
-                (e + 1) * numOfContentsInEachRaw,
-              )
-              .map(content => {
-                if (content)
-                  return (
-                    <Slider.Item
-                      movie={content._source}
-                      key={content._source.video_id}
-                    />
-                  );
-              })}
-          </Slider>
-        );
-      })}
+      {noContents ? (
+        <MessageOnCenter>검색 결과가 없습니다.</MessageOnCenter>
+      ) : (
+        sliceIndexArray.map(e => {
+          return (
+            <Slider>
+              {myVideoList
+                .slice(
+                  e * numOfContentsInEachRaw,
+                  (e + 1) * numOfContentsInEachRaw,
+                )
+                .map(content => {
+                  if (content)
+                    return (
+                      <Slider.Item
+                        movie={content._source}
+                        key={content._source.video_id}
+                      />
+                    );
+                })}
+            </Slider>
+          );
+        })
+      )}
+      {}
     </>
   );
 };
--- elastic-server/build/logstash_init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /usr/share/logstash/bin
-./logstash -f logstash-mysql.conf --path.data /usr/share/logstash/data/1
-node object_storage.js
