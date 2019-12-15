50eadb0d20849c70820567fab6e0fcaa3562efd8
jngsoo <wt2933@icloud.com>
2019-12-16 01:02:44 +0900

    Revise: 태그 크기 h4 => h5 수정
    태그가 너무 많은 콘텐츠들 때문에 h5로 더 작게 수정

--- client-server/src/components/Tag/Tag.js
+++ client-server/src/components/Tag/Tag.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const Container = styled.h4`
+const Container = styled.h5`
   display: inline-block;
   border: solid 0.05rem lightgray;
   border-radius: 0.2rem;
