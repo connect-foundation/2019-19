7ac63ba8349db436bb07ec3864cb947ee50ce160
zoomspeed77 <zoomspeed77@gmail.com>
2019-12-16 20:43:29 +0900

    Fix: Index에서 한국어, 영어 검색 제대로 안나오는 버그 수정

---
+++ elastic-server/src/utils/createIndex.js
@@ -0,0 +1,141 @@
+const { client } = require('./elastic_connection.js');
+const index = process.env.index;
+console.log("index : ",index);
+const type = '_doc';
+(function init() {
+  Promise.resolve()
+    .then(deleteIndex, handleError)
+    .then(createIndex, handleError)
+    .then(checkStatus, handleError)
+    .then(closeIndex, handleError)
+    .then(putSettings, handleError)
+    .then(putMapping, handleError)
+    .then(openIndex, handleError);
+})();
+function deleteIndex() {
+  console.log('Deleting old index ...');
+  return client.indices.delete({
+    index: index,
+    ignore: [404]
+  }).then(handleResolve);
+}
+function createIndex() {
+  console.log('Creating new index ...');
+  return client.indices.create({
+    index: index,
+    body: {
+      settings: {
+        index: {
+          number_of_replicas: 0 // for local development
+        }
+      }
+    }
+  }).then(handleResolve);
+}
+// This isn't strictly necessary, but it solves a problem with closing
+// the index before it has been created
+function checkStatus() {
+  console.log('Checking status ...');
+  return client.cluster.health({
+    index: index
+  }).then(handleResolve);
+}
+function closeIndex() {
+  console.log('Closing index ...');
+  return client.indices.close({
+    index: index
+  }).then(handleResolve);
+}
+function putSettings() {
+  console.log('Put settings ...');
+  return client.indices.putSettings({
+    index: index,
+    body: {
+      settings: {
+        analysis: {
+          analyzer: {
+            autocomplete: {
+              tokenizer: 'autocomplete',
+              filter: ['lowercase']
+            },
+            autocomplete_search: {
+              tokenizer: 'lowercase'
+            },
+            korean: {
+              filter: ['npos_filter', 'nori_readingform', 'lowercase'],
+              tokenizer: "nori_user_dict"
+            }
+          },
+          tokenizer: {
+            autocomplete: {
+              type: 'edge_ngram',
+              min_gram: 1,
+              max_gram: 30,
+              token_chars: [
+                'letter',
+                'digit'
+              ]
+            },
+            nori_user_dict: {
+              mode: "mixed",
+              type: "nori_tokenizer",
+              user_dictionary: "userdic_ko.txt"
+            }
+          },
+          filter: {
+            npos_filter: {
+              type: "nori_part_of_speech",
+              stoptags: [
+                "E", "IC", "J", "MAG", "MM", "SP", "SSC",
+                "SSO", "SC", "SE", "XPN", "XSA",
+                "XSN", "XSV", "UNA", "NA", "VSV"
+              ]
+            }
+          }
+        }
+      }
+    }
+  }).then(handleResolve);
+}
+function putMapping() {
+  console.log('Put mapping ...');
+  return client.indices.putMapping({
+    index: index,
+    body: {
+      _source: {
+        enabled: true
+      },
+      properties: {
+        name: { type: 'text', analyzer: 'autocomplete', search_analyzer: "autocomplete" },
+        name_ko: { type: 'text', analyzer: 'korean', search_analyzer: "korean" },
+        likes: { type: 'long' },
+        reg_date: { type: 'date' },
+        streaming_url: { type: 'text' },
+        thumbnai_video_url: { type: 'text' },
+        thumbnail_img_url: { type: 'text' },
+        video_id: { type: 'text' },
+        //category: { type: 'text', analyzer: 'autocomplete', search_analyzer: "autocomplete" },
+        category: { type: 'text', analyzer: 'korean', search_analyzer: "korean" },
+        index : {type: 'long'}
+      }
+    }
+  }).then(handleResolve);
+}
+function openIndex() {
+  console.log('Open index ...');
+  return client.indices.open({
+    index: index
+  }).then(handleResolve);
+}
+function handleResolve(body) {
+  if (!body.error) {
+    console.log('\x1b[32m' + 'Success' + '\x1b[37m');
+  } else {
+    console.log('\x1b[33m' + 'Failed' + '\x1b[37m');
+  }
+  return Promise.resolve();
+}
+function handleError(err) {
+  console.error(JSON.stringify(err.body, null, 2));
+  return Promise.reject();
+}
