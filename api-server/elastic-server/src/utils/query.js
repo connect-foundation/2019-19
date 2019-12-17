8e17e334b92134ebb221f7a085287388d884cb28
minsung1129 <sju02135@hanmail.net>
2019-12-17 11:13:03 +0900

    Fix: query bug fix
    다른 브랜치와 충돌나는 부분 수정
    Elastic search sort부분 수정

--- api-server/elastic-server/src/utils/query.js
+++ api-server/elastic-server/src/utils/query.js
@@ -31,15 +31,10 @@ async function get_filtering(column, order, size) {
  * @param {*} option ex)  [{ reg_date: { order: 'desc' } }]
  */
 async function get_category(category_list, order, size, sort) {
-
   return await client
     .search({
       index: process.env.index,
       type: '_doc',
-<<<<<<< HEAD
-=======
-      sort: [`${column}:${order}`],
->>>>>>> 4b0421fac1cef363bd71130e6eea9c9650d24505
       body: {
         sort: sort,
         size: size,
@@ -68,11 +63,7 @@ async function get_search(column, target, order, size) {
     .search({
       index: process.env.index,
       type: '_doc',
-<<<<<<< HEAD
       sort: [`${column} : ${order}`],
-=======
-      sort: [`${column}:${order}`],
->>>>>>> 4b0421fac1cef363bd71130e6eea9c9650d24505
       body: {
         size: size,
         query: {
