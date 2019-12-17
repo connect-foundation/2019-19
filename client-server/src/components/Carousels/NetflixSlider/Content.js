7d7d353ae9ce83145fa06ef5b243bffe7a6984df
jngsoo <wt2933@icloud.com>
2019-12-17 12:37:45 +0900

    Add: 콘텐츠 상세 정보에 표시되는 태그 개수 제한
    기존 모든 태그를 보여주면 너무 많은 태그가 보여져 UI가 깨지는 현상이 발생해
    최대 30개의 태그만 보여주도록 수정
    아울러 태그 정보 요청 API 응답을 기다리는 동안 태그가 보여지는 자리에 로딩 UI가 보여지도록 추가

--- client-server/src/components/Carousels/NetflixSlider/Content.js
+++ client-server/src/components/Carousels/NetflixSlider/Content.js
@@ -2,8 +2,11 @@
 import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import { ClipLoader } from 'react-spinners';
+import { css } from '@emotion/core';
 import IconCross from '../Icons/IconCross';
 import PlayButton from '../../PlayButton';
+
 import LikeBtn from '../../like/like';
 import MylistBtn from '../../like/mylist';
 import LoginContext from '../../../loginContextApi/context';
@@ -17,9 +20,11 @@ const apiServer = ENV.apiServer;
 const Content = ({ movie, onClose }) => {
   const { userInfo } = useContext(LoginContext);
   const [tags, setTags] = useState(null);
+  const [tagsOnLoading, setTagsOnLoading] = useState(true);
   useEffect(() => {
     axios.get(`${apiServer}/video/tags/${movie.video_id}`).then(tagsData => {
       setTags(tagsData.data);
+      setTagsOnLoading(false);
     });
     // Bind the event listener
     document.getElementById(`content-${movie.video_id}`).addEventListener(
@@ -53,7 +58,19 @@ const Content = ({ movie, onClose }) => {
         <div className="content__area__container">
           <div className="content__title">{movie.name}</div>
           <TagsContainer>
-            {tags && tags.map(tag => <Tag name={tag.name} />)}
+            {tagsOnLoading ? (
+              <ClipLoader
+                css={css`
+                  margin: 2% 48%;
+                `}
+                sizeUnit="rem"
+                size={3}
+                color="lightgray"
+                loading={tagsOnLoading}
+              />
+            ) : (
+              tags.slice(0, 30).map(tag => <Tag name={tag.name} />)
+            )}
           </TagsContainer>
           <div className="content__btns__container">
             <PlayButton name="▶  재생" videoId={movie.video_id} />
